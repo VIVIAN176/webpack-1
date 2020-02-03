c00a2c9f03ca0edf1d6ce1925eb6037075f051d4
zhangdeshou <xjlady@126.com>
2020-02-04 00:45:53 +0800

    完成目标4.webpack引入css

--- src/x.js
+++ src/x.js
@@ -1,1 +1,5 @@
+import css from './x.css'
+console.log(css.toString())
 console.log('xxx')
+
+export default 'xxx'
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
 module.exports = {
@@ -7,5 +8,21 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         // filename: "index.js", // string
         filename: '[name].[contenthash].js',
-    }
+        // filename: 'xxxxxxxxxx.[contenthash].js',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'webpack.config.js配置里面的title',
+        template: 'src/assets/index.html' //以这个文件为模板生成dist中的Html页面
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                //css-loader把css文件读到js里面
+                //style-loader把读到的内容插入到style标签里面
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
 }
+
